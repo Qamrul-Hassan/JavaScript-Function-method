99103a96888fe321d3cf3464b9612073d28e2253
Qamrul-Hassan <sajal700@gmail.com>
2024-10-29 00:44:59 +0600

    massage

--- Q1.js
+++ Q1.js
@@ -5,8 +5,16 @@ const names = ["Alice", "Bob", "John", "Rock"];
 const greetings = names.map(name => `Hello, ${name}`);
 console.log(greetings)
 
-/* 2.Multiply each number in an array by 2*/
+/* 2.Multiply each number in an array by 2
+ use map() to create a new array where each number is doubled:*/
 
 const  numbers = [1, 2, 3, 4, 5];
 const evenNumbers = numbers.map(number => number * 2);
 console.log(evenNumbers);
+
+/* 3. Convert an array of numbers to their string representations
+This can also be done with map() by converting each number to a string: */
+
+const nos = [1, 2, 3, 4, 5,];
+const stringNos =nos.map (no => no.toString());
+console.log(stringNos);
