028cd2f45685a14d8b1568a593a9041aa79f54e9
Qamrul-Hassan <sajal700@gmail.com>
2024-10-29 00:38:42 +0600

    massage

--- Q1.js
+++ Q1.js
@@ -4,3 +4,9 @@ Example: Prefix each name in an array with "Hello, ".*/
 const names = ["Alice", "Bob", "John", "Rock"];
 const greetings = names.map(name => `Hello, ${name}`);
 console.log(greetings)
+
+/* 2.Multiply each number in an array by 2*/
+
+const  numbers = [1, 2, 3, 4, 5];
+const evenNumbers = numbers.map(number => number * 2);
+console.log(evenNumbers);
