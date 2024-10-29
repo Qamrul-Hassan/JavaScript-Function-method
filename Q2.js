2dc03fb9099e8c0ffa7b31aaf5cc9854973a2628
Qamrul-Hassan <sajal700@gmail.com>
2024-10-30 01:09:17 +0600

    massage

--- Q2.js
+++ Q2.js
@@ -17,3 +17,18 @@ Use the filter() method to keep only even numbers in a new array:*/
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const evenNumbers = numbers.filter(number => number % 2 ===0);
 console.log(evenNumbers);
+
+/* Get an array of students who passed (score > 50) from a list of students 
+This example uses filter() to keep only students with scores greater than 50:*/
+
+const students = [
+   { name: "Arif", score: "45" },
+   { name: " Sohan", score: "75"},
+   { name: "Karim", scroe: "60"},
+   { name: "Rahim", score: "30"},
+   { name: "Jibon", score: "85"} 
+];
+
+const passingStudents = students.filter(student => student.score > 50);
+console.log(passingStudents);
+
